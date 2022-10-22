231cec6a6fbd9d1967bd71df2300fe8045ce42c7
augustosnk12 <augustosnk12@gmail.com>
2022-10-22 09:08:30 -0300

    inital commit

---
+++ src/users.js
@@ -0,0 +1,37 @@
+export const users = [
+	{
+		name: 'Pedro Lucas',
+		skill: 'Kissing pigs',
+		birthdate: new Date('1999-12-24 03:00:00'),
+		age: 20,
+		active: true,
+	},
+	{
+		name: 'Dirceu We Read',
+		skill: 'Plant Cactus',
+		birthdate: new Date('1970-10-01 03:00:00'),
+		age: 30,
+		active: true,
+	},
+	{
+		name: 'Rodrigo KissHimself',
+		skill: 'Find bugs',
+		birthdate: new Date('1990-01-30 03:00:00'),
+		age: 15,
+		active: true,
+	},
+	{
+		name: 'Ronaldo AraGrain',
+		skill: 'All you can imagine!!!',
+		birthdate: new Date('3000-12-12 03:00:00'),
+		age: NaN,
+		active: true,
+	},
+	{
+		name: 'Paiva no lastname',
+		skill: 'Not finishing novels',
+		birthdate: new Date('2000-05-10 03:00:00'),
+		age: 25,
+		active: false,
+	},
+];
